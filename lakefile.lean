bebebef59d611cd29488d70e7ccc5bd2d5e43e93
bushwallyta271828 <aheckett@gmail.com>
2025-09-05 12:20:12 -0400

    _synchronize_github

---
+++ lakefile.lean
@@ -0,0 +1,32 @@
+import Lake
+open Lake DSL
+
+package «agora_lib» {
+  -- add any package configuration options here
+}
+
+-- require mathlib from git
+--   "https://github.com/leanprover-community/mathlib4.git" @ "v4.17.0"
+
+-- require Batteries from git
+--   "https://github.com/leanprover-community/mathlib4.git" @ "v4.17.0"
+
+require "leanprover-community" / "batteries" @ git "v4.17.0"
+
+require "leanprover" / "Cli" @ git "v4.17.0"
+
+
+
+@[default_target]
+lean_lib Library where
+
+@[default_target]
+lean_lib Targets where
+
+
+lean_lib Utils where
+
+@[default_target]
+lean_exe get_theorems where
+  root := `get_theorems
+  supportInterpreter := true
