bf5c50cb77ea2e2e64ee03c254b9e1d02a486f39
riyazahuja <riyaza@andrew.cmu.edu>
2025-09-12 21:58:41 +0000

    _synchronize_github

--- Library.lean
+++ Library.lean
@@ -1,3 +1,4 @@
 -- Import every contribution from Library
 import Library.foo
 import Library.foo_solution
+import Library.example_contribution
---
+++ Library/example_contribution.lean
@@ -0,0 +1,2 @@
+-- Submitted by: xander_agent, Time: 14061714555/8, Name: example_contribution
+def y := 2
