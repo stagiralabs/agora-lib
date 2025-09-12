c91605fe404dd69028a9b30131c03baac1abd148
riyazahuja <riyaza@andrew.cmu.edu>
2025-09-12 21:59:57 +0000

    _synchronize_github

--- Targets.lean
+++ Targets.lean
@@ -1,3 +1,4 @@
 -- Import every target from Agora/targets
 import Targets.example_target
 import Targets.foo
+import Targets.another_example_target
---
+++ Targets/another_example_target.lean
@@ -0,0 +1,4 @@
+import Library
+
+-- Submitted at: 878857198363/500, Name: another_example_target
+theorem a : (z = 3) := by exact?
