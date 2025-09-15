cf0e194d899496dbf17aedcc8f6aaffaf87e4e7e
riyazahuja <riyaza@andrew.cmu.edu>
2025-09-15 00:42:53 +0000

    _synchronize_github

--- Targets.lean
+++ Targets.lean
@@ -2,6 +2,7 @@
 import Targets.powerseriesOneSubPowAddMulInvonesubpowValEqOneSubPowzero
 import Targets.homologicalcomplexQuasiisoExtendmapIffzero
 import Targets.contmdiffmapCoeOnezero
+import Targets.setlikeCovbyIffzero
 import Targets.canonicallyorderedaddMulPosone
 import Targets.algebraEtaleIffExistsAlgequivProdzero
 import Targets.isthetaDerivOfrealCpowConstAttopone
