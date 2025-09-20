2799f5edf7e95c99898ce6e3c9b201f12f8fb2a6
riyazahuja <riyaza@andrew.cmu.edu>
2025-09-20 09:28:50 +0000

    _synchronize_github

--- Targets.lean
+++ Targets.lean
@@ -1,3 +1,4 @@
 -- Import every target from Agora/targets
 import Targets.homologicalcomplexQuasiisoExtendmapIffzero
+import Targets.isapproximatesubgroupPowInterPowCovbysmulSqInterSqzero
 import Targets.setlikeIscoatomIffone
