07eecd962d6d301f68094173de1d23a6983c739f
riyazahuja <riyaza@andrew.cmu.edu>
2025-09-20 04:18:48 +0000

    _synchronize_github

--- Targets.lean
+++ Targets.lean
@@ -1,4 +1,5 @@
 -- Import every target from Agora/targets
+import Targets.listReduceoptionEqSingletonIffzero
 import Targets.powerseriesOneSubPowAddMulInvonesubpowValEqOneSubPowzero
 import Targets.homologicalcomplexQuasiisoExtendmapIffzero
 import Targets.setlikeCovbyIffzero
