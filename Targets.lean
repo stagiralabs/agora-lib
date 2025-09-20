ead9a9be7ff897e3b7f2e731c5d4d5ea99a1e45f
riyazahuja <riyaza@andrew.cmu.edu>
2025-09-20 13:00:25 +0000

    _synchronize_github

--- Targets.lean
+++ Targets.lean
@@ -5,6 +5,7 @@ import Targets.contmdiffmapCoeOnezero
 import Targets.setlikeCovbyIffzero
 import Targets.canonicallyorderedaddMulPosone
 import Targets.algebraEtaleIffExistsAlgequivProdzero
+import Targets.matrixDetUpdatecolSumzero
 import Targets.zlatticeCovolumeTendstoCardLeDivone
 import Targets.canonicallyorderedaddPowPoszero
 import Targets.privateZlatticeCovolumeFrontierEquivfunzero
