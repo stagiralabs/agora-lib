40d1452b3a0f8ac54868c97046ed6ffc3c5b46ff
riyazahuja <riyaza@andrew.cmu.edu>
2025-09-20 07:21:04 +0000

    _synchronize_github

--- Targets.lean
+++ Targets.lean
@@ -12,5 +12,6 @@ import Targets.setPairwisedisjointIffone
 import Targets.isapproximatesubgroupCardMulSelfLeone
 import Targets.setlikeIscoatomIffone
 import Targets.powerseriesInvonesubpowAddone
+import Targets.homologicalcomplexExtendhomologyisoHomNaturalityone
 import Targets.charactermoduleDualInjectiveIffSurjectivezero
 import Targets.matrixDetUpdatecolEqZeroone
