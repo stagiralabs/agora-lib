07591e6e4cf8e569d116dad8d741bf28979e6728
riyazahuja <riyaza@andrew.cmu.edu>
2025-09-20 10:48:55 +0000

    _synchronize_github

--- Targets.lean
+++ Targets.lean
@@ -13,6 +13,7 @@ import Targets.isapproximatesubgroupPowInterPowCovbysmulSqInterSqzero
 import Targets.setPairwisedisjointIffone
 import Targets.derivNormOfrealCpowzero
 import Targets.setPairwisedisjointPairInsertzero
+import Targets.isapproximatesubgroupCardMulSelfLeone
 import Targets.setlikeIscoatomIffone
 import Targets.powerseriesInvonesubpowAddone
 import Targets.polynomialHilbertpolyEqZeroOfLeRootmultiplicityOneone
