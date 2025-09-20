f4168b824da88ca28f09c6cbe8e0c7b0b1960a74
riyazahuja <riyaza@andrew.cmu.edu>
2025-09-20 09:13:24 +0000

    _synchronize_github

--- Targets.lean
+++ Targets.lean
@@ -1,3 +1,4 @@
 -- Import every target from Agora/targets
+import Targets.canonicallyorderedaddPowPoszero
 import Targets.isapproximatesubgroupCardMulSelfLeone
 import Targets.charactermoduleDualInjectiveIffSurjectivezero
