53dc9d0f7b36b0a5bf223537251be7a7d2662423
riyazahuja <riyaza@andrew.cmu.edu>
2025-09-20 08:34:01 +0000

    _synchronize_github

--- Targets.lean
+++ Targets.lean
@@ -4,4 +4,5 @@ import Targets.algebraEtaleIffExistsAlgequivProdzero
 import Targets.canonicallyorderedaddPowPoszero
 import Targets.strictconvexspaceOfStrictconvexUnitclosedballzero
 import Targets.isapproximatesubgroupCardMulSelfLeone
+import Targets.setlikeIscoatomIffone
 import Targets.charactermoduleDualInjectiveIffSurjectivezero
