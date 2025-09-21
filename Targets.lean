91f070c6d3a2730e3adef95c207a75e190e79d31
riyazahuja <riyaza@andrew.cmu.edu>
2025-09-21 01:13:06 +0000

    _synchronize_github

--- Targets.lean
+++ Targets.lean
@@ -5,6 +5,7 @@ import Targets.algebraEtaleIffExistsAlgequivProdzero
 import Targets.isthetaDerivOfrealCpowConstAttopone
 import Targets.matrixDetUpdatecolSumzero
 import Targets.zlatticeCovolumeTendstoCardLeDivone
+import Targets.isapproximatesubgroupPowInterPowCovbysmulSqInterSqzero
 import Targets.setPairwisedisjointIffone
 import Targets.setPairwisedisjointPairInsertzero
 import Targets.setlikeIscoatomIffone
