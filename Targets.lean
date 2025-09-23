e0a748aa54761f146f13f39d5800c983f32ae55a
riyazahuja <riyaza@andrew.cmu.edu>
2025-09-23 03:08:10 +0000

    _synchronize_github

--- Targets.lean
+++ Targets.lean
@@ -1,6 +1,7 @@
 -- Import every target from Agora/targets
 import Targets.listReduceoptionEqSingletonIffzero
 import Targets.isthetaDerivOfrealCpowConstAttopone
+import Targets.zlatticeCovolumeTendstoCardLeDivone
 import Targets.derivNormOfrealCpowzero
 import Targets.isapproximatesubgroupCardMulSelfLeone
 import Targets.charactermoduleDualInjectiveIffSurjectivezero
